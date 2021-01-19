947ab661a1a737c2bd5c49525f80979b11145485
Sabrina Marzuki <56462666+sbrnmrzk@users.noreply.github.com>
2021-01-20 07:14:09 +0800

    Revert "Try resetting branch to fatihah's changes"

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
-const express = require('express');
+var express = require('express');
+const multer = require('multer');
+const db = require("./db/index");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -33,6 +35,41 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+//================== FOR UPLOADING IMAGE PURPOSES ==================
+
+const storage = multer.diskStorage({
+  destination: "./../frontend/src/images/uploads/",
+  filename: function(req, file, cb){
+     cb(null,"IMAGE-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+app.patch('/users/edit/:userID', (req, res) => {
+  console.log(req.body);
+  const user_id = req.body.user_id;
+  const text = 'UPDATE users SET (username, email, first_name, last_name, user_img) = ($1, $2, $3, $4, $5) WHERE user_id=($6)'
+  const values = [req.body.username, req.body.email, req.body.first_name, req.body.last_name, "IMAGE-" + req.body.imageInput, req.body.user_id];
+
+  db.query(text, values)
+       .then(() => {
+        console.log("success!");
+        // success;
+      })
+      .catch(error => {
+        console.log(error);
+});
+  upload(req, res, (err) => {
+    if (err) {
+      res.sendStatus(500);
+    }
+    res.send(req.file);
+  });
+});
+
+// =================================================================
 
 app.get('/isloggedin', function(req, res) {
     if(req.session.passport.user) {
